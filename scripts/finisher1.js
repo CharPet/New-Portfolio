adda253a358e609a4a9d71ec13535e1444bfba35
CharPet <137006290+CharPet@users.noreply.github.com>
2025-06-12 16:29:40 +0300

    Added pdf's

--- scripts/finisher1.js
+++ scripts/finisher1.js
@@ -1,3 +1,38 @@
+// // ===============================
+// // Finisher Header Initialization
+// // ===============================
+// if (typeof FinisherHeader !== "undefined") {
+//   new FinisherHeader({
+//     count: 70,
+//     size: {
+//       min: 1300,
+//       max: 1500,
+//       pulse: 0,
+//     },
+//     speed: {
+//       x: {
+//         min: 0.1,
+//         max: 0.6,
+//       },
+//       y: {
+//         min: 0.1,
+//         max: 0.6,
+//       },
+//     },
+//     colors: {
+//       background: "#9138e5",
+//       particles: ["#ff4848", "#000000", "#2235e5", "#000000", "#ff0000"],
+//     },
+//     blending: "overlay",
+//     opacity: {
+//       center: 0.5,
+//       edge: 0.05,
+//     },
+//     skew: 0,
+//     shapes: ["c"],
+//   });
+// }
+
 // ===============================
 // Finisher Header Initialization
 // ===============================
@@ -12,23 +47,81 @@ if (typeof FinisherHeader !== "undefined") {
     speed: {
       x: {
         min: 0.1,
-        max: 0.6,
+        max: 1.5,
       },
       y: {
         min: 0.1,
-        max: 0.6,
+        max: 3.5,
       },
     },
     colors: {
-      background: "#9138e5",
-      particles: ["#ff4848", "#000000", "#2235e5", "#000000", "#ff0000"],
+      background: "#d9d9d9",
+      particles: [
+        "#d9d9d9",
+        "#262e53",
+        "#c26b57",
+        "#eef3f5",
+        "#7D86A3",
+        "#292929",
+        "#d9d9d9",
+        "#262e53",
+        // "#c26b57",
+        "#eef3f5",
+        // "#7D86A3",
+        "#292929",
+        "#262e53",
+        // "#c26b57",
+        "#eef3f5",
+        // "#7D86A3",
+        "#292929",
+      ],
     },
     blending: "overlay",
     opacity: {
       center: 0.5,
-      edge: 0.05,
+      edge: 0.01,
     },
     skew: 0,
     shapes: ["c"],
   });
 }
+
+// if (typeof FinisherHeader !== "undefined") {
+//   new FinisherHeader({
+//     count: 70,
+//     size: {
+//       min: 1300,
+//       max: 1500,
+//       pulse: 0,
+//     },
+//     speed: {
+//       x: {
+//         min: 0.1,
+//         max: 0.6,
+//       },
+//       y: {
+//         min: 0.1,
+//         max: 0.6,
+//       },
+//     },
+//     colors: {
+//       background: "#C0C0C0",
+//       particles: [
+//         "#C0C0C0",
+//         "#000000",
+//         "#228B22",
+//         "#FFFFFF",
+//         "#D4AF37",
+//         "#FF0000",
+//         // "#FFFF00",
+//       ],
+//     },
+//     blending: "overlay",
+//     opacity: {
+//       center: 0.5,
+//       edge: 0.01,
+//     },
+//     skew: 0,
+//     shapes: ["c"],
+//   });
+// }
