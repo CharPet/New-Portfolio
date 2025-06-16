79f9ef45570fd2499b03ddc3078cf84feb66bcca
CharPet <137006290+CharPet@users.noreply.github.com>
2025-06-16 13:19:59 +0300

    Added support for .planet-grid at max-width <= 320px (Update 11).

--- scripts/finisher1.js
+++ scripts/finisher1.js
@@ -1,37 +1,37 @@
 // // ===============================
 // // Finisher Header Initialization
 // // ===============================
-// if (typeof FinisherHeader !== "undefined") {
-//   new FinisherHeader({
-//     count: 70,
-//     size: {
-//       min: 1300,
-//       max: 1500,
-//       pulse: 0,
-//     },
-//     speed: {
-//       x: {
-//         min: 0.1,
-//         max: 0.6,
-//       },
-//       y: {
-//         min: 0.1,
-//         max: 0.6,
-//       },
-//     },
-//     colors: {
-//       background: "#9138e5",
-//       particles: ["#ff4848", "#000000", "#2235e5", "#000000", "#ff0000"],
-//     },
-//     blending: "overlay",
-//     opacity: {
-//       center: 0.5,
-//       edge: 0.05,
-//     },
-//     skew: 0,
-//     shapes: ["c"],
-//   });
-// }
+if (typeof FinisherHeader !== "undefined") {
+  new FinisherHeader({
+    count: 70,
+    size: {
+      min: 1300,
+      max: 1500,
+      pulse: 0,
+    },
+    speed: {
+      x: {
+        min: 0.1,
+        max: 0.6,
+      },
+      y: {
+        min: 0.1,
+        max: 0.6,
+      },
+    },
+    colors: {
+      // background: "#9138e5",
+      particles: ["#ff4848", "#000000", "#2235e5", "#000000", "#ff0000"],
+    },
+    blending: "overlay",
+    opacity: {
+      center: 0.5,
+      edge: 0.05,
+    },
+    skew: 0,
+    shapes: ["c"],
+  });
+}
 
 // ===============================
 // Finisher Header Initialization
@@ -112,6 +112,146 @@ if (typeof FinisherHeader !== "undefined") {
 //     opacity: {
 //       center: 0.5,
 //       edge: 0.01,
+//     },
+//     skew: 0,
+//     shapes: ["c"],
+//   });
+// }
+
+if (typeof FinisherHeader !== "undefined") {
+  new FinisherHeader({
+    count: 70,
+    size: {
+      min: 1300,
+      max: 1500,
+      pulse: 0,
+    },
+    speed: {
+      x: {
+        min: 0.1,
+        max: 1,
+      },
+      y: {
+        min: 0.1,
+        max: 1,
+      },
+    },
+    colors: {
+      background: "#fffcf2",
+      particles: [
+        "#fffcf2",
+        "#ccc5b9",
+        "#403d39",
+        "#252422",
+        "#eb5e28",
+        "#14213d",
+        "#ffffff",
+        "#14213d",
+      ],
+    },
+    blending: "overlay",
+    opacity: {
+      center: 0.3,
+      edge: 0.02,
+    },
+    skew: 0,
+    shapes: ["c"],
+  });
+}
+
+// if (typeof FinisherHeader !== "undefined") {
+//   new FinisherHeader({
+//     count: 70,
+//     size: {
+//       min: 1300,
+//       max: 1500,
+//       pulse: 0,
+//     },
+//     speed: {
+//       x: {
+//         min: 0.1,
+//         max: 1,
+//       },
+//       y: {
+//         min: 0.1,
+//         max: 1,
+//       },
+//     },
+//     colors: {
+//       // background: "#ffffff",
+//       particles: [
+//         "#0092a0",
+//         "#12de97",
+//         "#040358",
+//         "#260f36",
+//         "#f63f44",
+//         "#902528",
+//         "#e59e6f",
+//         "#8ba78d",
+//       ],
+//     },
+//     blending: "overlay",
+//     opacity: {
+//       center: 0.3,
+//       edge: 0.02,
+//     },
+//     skew: 0,
+//     shapes: ["c"],
+//   });
+// }
+// :root {
+//   --shark-black: #212529;
+//   --text-white: #fefefe;
+//   --mojo-red: #c23a3e;
+//   --outline-gray: #808080;
+//   --dark-cyan: #0092a0;
+//   --lime-green: #12de97;
+//   --dark-blue: #040358;
+//   --dark-violet: #260f36;
+//   --sunset-red: #f63f44;
+//   --burnt-umber-red: #902528;
+//   --apricot-orange: #e59e6f;
+//   --envy-green: #8ba78d;
+//   --validation-green: #28a745;
+//   --chenin: #ded582;
+//   --input-background-white: #fff0f0;
+// }
+
+// if (typeof FinisherHeader !== "undefined") {
+//   new FinisherHeader({
+//     count: 70,
+//     size: {
+//       min: 1300,
+//       max: 1500,
+//       pulse: 0,
+//     },
+//     speed: {
+//       x: {
+//         min: 0.1,
+//         max: 1,
+//       },
+//       y: {
+//         min: 0.1,
+//         max: 1,
+//       },
+//     },
+//     colors: {
+//       // background: "#ffffff",
+//       particles: [
+//         "#2C3639",
+//         "#3F4E4F",
+//         "#A27B5C",
+//         "#DCD7C9",
+//         // "#3c78c2",
+//         // "#902528",
+//         // "#e59e6f",
+//         // "#8ba78d",
+//       ],
+//     },
+//     blending: "overlay",
+//     opacity: {
+//       center: 0.3,
+//       edge: 0.02,
 //     },
 //     skew: 0,
 //     shapes: ["c"],
